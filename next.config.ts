b1e33d263a61db640b86c401f0fdf492dbc9f697
Muhammad Zakaa Shahzada <09021182328014@student.unsri.ac.id>
2026-07-06 16:21:12 +0700

    next.conf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb', // Support uploading PDF files larger than 1MB
+    },
+  },
 };
 
 export default nextConfig;
